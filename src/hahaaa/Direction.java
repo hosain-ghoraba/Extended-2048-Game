b695c3a971a47d7e96bbb096a8ff877f2129aacb
hosain ghoraba <hussain.ghoraba@gmail.com>
2023-03-01 20:57:30 +0200

    first 2048 Game commit

---
+++ src/hahaaa/Direction.java
@@ -0,0 +1,11 @@
+package hahaaa;
+public enum Direction
+{
+    UP("UP", 0), 
+    DOWN("DOWN", 1), 
+    RIGHT("RIGHT", 2), 
+    LEFT("LEFT", 3);
+    
+    private Direction(final String s, final int n) {
+    }
+}
